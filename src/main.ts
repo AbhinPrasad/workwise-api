171fa7fb3b17df8dffb362f79faf7c6f869fd98e
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-03-31 20:11:59 +0530

    docs: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config'
 import { ValidationPipe, VersioningType } from '@nestjs/common'
 import validationOptions from './common/utils/validation-options'
 import { ResponseInterceptor } from './common/interceptors/response.interceptor'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -14,6 +15,14 @@ async function bootstrap() {
   })
   app.useGlobalPipes(new ValidationPipe(validationOptions))
   app.useGlobalInterceptors(new ResponseInterceptor())
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API docs')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('docs', app, document)
   const port = configService.get<string>('app.port') ?? 8081
   await app.listen(port)
   console.log(`server running on ${await app.getUrl()}`)

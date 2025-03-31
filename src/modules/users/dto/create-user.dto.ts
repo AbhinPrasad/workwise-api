889b931fa8eb4baddebf2d0f955977ca00047028
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-03-31 20:34:51 +0530

    feat: create user dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,3 +1,28 @@
-import { IsString } from 'class-validator'
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsFirebasePushId,
+  IsUrl,
+} from 'class-validator'
 
-export class CreateUserDto {}
+export class CreateUserDto {
+  @IsFirebasePushId()
+  uid: string
+
+  @IsString()
+  @IsOptional()
+  userName: string
+
+  @IsString()
+  @IsEmail()
+  email: string
+
+  @IsString()
+  @IsOptional()
+  password: string
+
+  @IsUrl()
+  @IsOptional()
+  profileImgUrl: string
+}
